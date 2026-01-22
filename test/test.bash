3b6074541f9ce41745fe2b80c1e3b6a1222e5545
Yusaku Washio <washioyusaku@icloud.com>
2026-01-22 16:17:08 +0900

    fix:  タイムアウト延長

--- test/test.bash
+++ test/test.bash
@@ -11,11 +11,11 @@ colcon build --packages-select base64_encoder
 [ -f "$dir/.bashrc" ] && source "$dir/.bashrc"
 source install/setup.bash
 
-timeout 20s ros2 run base64_encoder base64-encoder-topic > /tmp/base64_node.log 2>&1 &
+timeout 60s ros2 run base64_encoder base64-encoder-topic > /tmp/base64_node.log 2>&1 &
 NODE_PID=$!
 trap 'kill $NODE_PID 2>/dev/null || true' EXIT
 
-timeout 20s ros2 topic echo --once /encoded std_msgs/msg/String --full-length > /tmp/encoded_out.txt &
+timeout 60s ros2 topic echo --once /encoded std_msgs/msg/String --full-length > /tmp/encoded_out.txt &
 ECHO_PID=$!
 
 
