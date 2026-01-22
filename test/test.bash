2021280e56d31f93995488ba91c5457b834b17b8
Yusaku Washio <washioyusaku@icloud.com>
2026-01-22 16:01:44 +0900

    fix: test.bashに分割

---
+++ test/test.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+dir=~
+[ "$1" != "" ] && dir="$1"
+
+cd "$dir/ros2_ws"
+
+source /opt/ros/humble/setup.bash
+colcon build --packages-select base64_encoder
+source "$dir/.bashrc"
+source install/setup.bash
+
+timeout 20s ros2 run base64_encoder base64-encoder-topic > /tmp/base64_node.log 2>&1 &
+NODE_PID=$!
+trap 'kill $NODE_PID 2>/dev/null || true' EXIT
+
+timeout 20s ros2 topic echo --once /encoded std_msgs/msg/String --full-length > /tmp/encoded_out.txt &
+ECHO_PID=$!
+
+
+ros2 topic pub --once /input_path std_msgs/String "{data: $dir/ros2_ws/src/base64_encoder/test/resources/ramen.jpg}"
+
+wait $ECHO_PID
+
+cat /tmp/encoded_out.txt | diff -u "$dir/ros2_ws/src/base64_encoder/test/resources/encoded_b64.txt" -
+
+kill $NODE_PID
+wait $NODE_PID || true
+
